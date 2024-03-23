9f55da3caa4336b4d0299c0a345d3f71317598d0
IvanSalovProg <ivansalov94@gmail.com>
2024-03-23 16:46:52 +0500

    Update Docs

--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -1,7 +1,23 @@
+import { ChangeEventHandler, EventHandler } from "react";
+
 const DocsPage = () => {
+const func1 = () => {
+  return 'Hello World!'
+}
+
+const param2 = "My property";
+
+const inputonChange = (event: any) => (
+console.log(event)
+)
+
+
+
   return (
     <div>
-      <p>This is umi docs.</p>
+      <p><input onChange= {inputonChange}/></p>
+      <p>{func1()}</p>
+      <p>{param2}</p>
     </div>
   );
 };
